2b644d5ef56889c2ac6b5362549e2a56c58a7d67
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-03-18 20:29:11 -0300

    Setup

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+
+const START_ADDR: u16 = 0x200;
+
+/// Emulator information handler
+pub struct Emu {
+    // Program Counter -> CPU current instruction index
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
+    // Stack Pointer -> Have the locale of the top of the stack
+    sp: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    // Delay Timer -> Used by the system as a typical timer. When hits 0 some action will be
+    // performad
+    dt: u8,
+    // Sound Timer -> When hits 0 will emmit some sound
+    st: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Emu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
     }
 }
